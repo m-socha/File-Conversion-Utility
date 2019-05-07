a4531f152e47a8775f379b9cdc3e7b703ba7c33b
Michael Socha <msocha@uwaterloo.ca>
2019-05-07 13:26:04 -0400

    Add conversion logic.

--- convert.py
+++ convert.py
@@ -1,4 +1,4 @@
-from os import listdir
+from os import listdir, rename
 from os.path import isfile
 import sys
 from enum import Enum
@@ -18,13 +18,36 @@ def print_usage():
   print('Usage: %s {%s}' % (src_file, ', '.join([e.value for e in Format])))
   print()
   print('The argument refers to the initial format (e.g. "panasonic" converts from panasonic to phone format).')
-  exit()
+  exit(1)
+
+def get_converted_filename(filename, src_format):
+  if src_format == Format.panasonic:
+    month = filename[:2]
+    day = filename[3:5]
+    year = filename[6:10]
+    time = filename[11:17]
+    return year + month + day + '_' + time + 'P.m2ts'
+  elif src_format == Format.phone:
+    year = filename[:4]
+    month = filename[4:6]
+    day = filename[6:8]
+    time = filename[9:15]
+    return month + '-' + day + '-' + year + '_' + time + '.m2ts'
 
 try:
   src_format = Format[sys.argv[1]]
 except:
   print_usage()
 
-files = [f for f in listdir() if isfile(f)]
+filenames = [f for f in listdir() if isfile(f)]
 regex = FORMAT_TO_REGEX[src_format]
-filtered_files = [f for f in files if regex.match(f)]
+filtered_filenames = [f for f in filenames if regex.match(f)]
+
+if not filtered_filenames:
+  print('No files found to convert.')
+  exit()
+
+converted_filenames = [get_converted_filename(f, src_format) for f in filtered_filenames]
+for filename, converted_filename in zip(filtered_filenames, converted_filenames):
+  rename(filename, converted_filename)
+  print('%s --> %s' % (filename, converted_filename))
