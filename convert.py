b3f3b9f6d5cca5877232ab3189f6b59b25eb0d6a
Michael Socha <msocha@uwaterloo.ca>
2019-05-07 12:58:35 -0400

    Add format regexes and filtering.

--- convert.py
+++ convert.py
@@ -2,23 +2,29 @@ from os import listdir
 from os.path import isfile
 import sys
 from enum import Enum
+import re
 
 class Format(Enum):
-  phone = 'phone'
   panasonic = 'panasonic'
+  phone = 'phone'
+
+FORMAT_TO_REGEX = {
+  Format.panasonic: re.compile('^\d{2}-\d{2}-\d{4}_\d{6}\.m2ts$'),
+  Format.phone: re.compile('^\d{8}_\d{6}P\.m2ts$'),
+}
 
 def print_usage():
   src_file = sys.argv[0]
   print('Usage: %s {%s}' % (src_file, ', '.join([e.value for e in Format])))
   print()
-  print('The argument refers to the destination format (e.g. "phone" converts from panasonic to phone format).')
+  print('The argument refers to the initial format (e.g. "panasonic" converts from panasonic to phone format).')
   exit()
 
 try:
-  dest_format = Format[sys.argv[1]]
+  src_format = Format[sys.argv[1]]
 except:
   print_usage()
 
-print(dest_format)
-
 files = [f for f in listdir() if isfile(f)]
+regex = FORMAT_TO_REGEX[src_format]
+filtered_files = [f for f in files if regex.match(f)]
