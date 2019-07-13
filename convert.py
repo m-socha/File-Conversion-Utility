e6555cee71579cd70a2a75cc9d2c77a26ead4573
Michael Socha <msocha@uwaterloo.ca>
2019-07-13 17:12:34 -0400

    Convert between filenames and times and add dest format.

--- convert.py
+++ convert.py
@@ -3,6 +3,7 @@ from os.path import isfile
 import sys
 from enum import Enum
 import re
+from datetime import datetime
 
 class Format(Enum):
   panasonic = 'panasonic'
@@ -15,27 +16,39 @@ FORMAT_TO_REGEX = {
 
 def print_usage():
   src_file = sys.argv[0]
-  print('Usage: %s {%s}' % (src_file, ', '.join([e.value for e in Format])))
+  formats_str = ', '.join([e.value for e in Format])
+  print('Usage: %s {%s}, {%s}' % (src_file, formats_str, formats_str))
   print()
-  print('The argument refers to the initial format (e.g. "panasonic" converts from panasonic to phone format).')
+  print('The first argument is the source format.')
+  print('The second argument is the destination format.')
   exit(1)
 
-def get_converted_filename(filename, src_format):
+def get_time_from_filename(filename, src_format):
   if src_format == Format.panasonic:
     month = filename[:2]
     day = filename[3:5]
     year = filename[6:10]
-    time = filename[11:17]
-    return year + month + day + '_' + time + 'p.m2ts'
+    hour = filename[11:13]
+    minute = filename[13:15]
+    second = filename[15:17]
   elif src_format == Format.phone:
     year = filename[:4]
     month = filename[4:6]
     day = filename[6:8]
-    time = filename[9:15]
-    return month + '-' + day + '-' + year + '_' + time + '.m2ts'
+    hour = filename[9:11]
+    minute = filename[11:13]
+    second = filename[13:15]
+  return datetime(int(year), int(month), int(day), int(hour), int(minute), int(second));
+
+def get_filename_from_time(time, dest_format):
+  if dest_format == Format.panasonic:
+    return time.strftime('%m-%d-%Y_%H%M%S') + '.m2ts'
+  elif dest_format == Format.phone:
+    return time.strftime('%Y%m%d_%H%M%S') + 'p.m2ts'
 
 try:
   src_format = Format[sys.argv[1]]
+  dest_format = Format[sys.argv[2]]
 except:
   print_usage()
 
@@ -47,7 +60,9 @@ if not filtered_filenames:
   print('No files found to convert.')
   exit()
 
-converted_filenames = [get_converted_filename(f, src_format) for f in filtered_filenames]
+times = [get_time_from_filename(f, src_format) for f in filtered_filenames]
+converted_filenames = [get_filename_from_time(t, dest_format) for t in times]
+
 for filename, converted_filename in zip(filtered_filenames, converted_filenames):
   rename(filename, converted_filename)
   print('%s --> %s' % (filename, converted_filename))
